89963d7d8e91bfeddf4514b1235ce00753ebfa6f
Ricardo Silva <47871470+ricardo2009@users.noreply.github.com>
2023-07-21 13:59:28 +0000

    teste

--- Terraform/modules/aks/locals.tf
+++ Terraform/modules/aks/locals.tf
@@ -5,5 +5,5 @@ locals {
   create_aks_identity = [for aks in var.aks : aks.name if aks.name != ""]
   ## dns prefix com condicional para cluster privado ou publico
   dns_prefix_private = try(var.aks[0].dns_prefix_private_cluster != null && var.aks[0].is_private_cluster ? var.aks[0].dns_prefix_private_cluster : var.aks[0].dns_prefix, null)
-  dns_prefix_public = try(var.dns_prefix, null)
+  dns_prefix_public = try(var.aks.dns_prefix, null)
 }
